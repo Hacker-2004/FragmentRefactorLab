55be628549f1a5cbbd4b98df874deb2eec5c0b53
hacker-2004 <tirth.ampatel@gmail.com>
2026-03-11 15:55:59 -0400

    added fragment

--- app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
@@ -5,24 +5,34 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import edu.temple.fragmentrefactor.databinding.FragmentGreetingActivityBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val displayTextView = findViewById<TextView>(R.id.displayTextView)
         val nameEditText = findViewById<EditText>(R.id.nameEditText)
         val changeButton = findViewById<Button>(R.id.changeButton)
 
-        changeButton.setOnClickListener {
-            val name = nameEditText.text
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView,
+                FragmentGreetingActivity.newInstance("Hello World")).commit()
 
-            displayTextView.text = if (name.isNotBlank()) {
-                "Hello, $name!"
-            } else {
-                "Please enter your name"
+            changeButton.setOnClickListener {
+                val name = nameEditText.text.toString()
+                val message = if (name.isEmpty()){
+                    "Please enter your name"
+                }
+                else{
+                    "Hello $name"
+                }
+
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView,
+                    FragmentGreetingActivity.newInstance(message)).commit()
             }
+
         }
+
     }
 }
